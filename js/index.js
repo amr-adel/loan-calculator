53f0e68f152945bdfffe4e35fe9a29a4e7b62808
Amr A. Abdelmoez <amradel88@live.com>
2019-07-17 23:00:20 +0200

    feat: Deploy service worker

--- js/index.js
+++ js/index.js
@@ -1,56 +1,51 @@
-import Form from "./models/Form";
-import {
-  getTransactionValue,
-  getInstallmentValue,
-  renderForm
-} from "./views/formView";
-import Result from "./models/Result";
-import { renderResult } from "./views/resultView";
-import { DOM } from "./views";
+import Form from './models/Form'
+import { getTransactionValue, getInstallmentValue, renderForm } from './views/formView'
+import Result from './models/Result'
+import { renderResult } from './views/resultView'
+import { DOM } from './views'
 
 // Global state
-const state = {};
+const state = {}
 
 // Form Controller
 const controlForm = () => {
-  if (!state.form) state.form = new Form();
+  if (!state.form) state.form = new Form()
 
-  state.form.transaction = +getTransactionValue();
-  state.form.minInstallment = state.form.calcMinInstallment();
+  state.form.transaction = +getTransactionValue()
+  state.form.minInstallment = state.form.calcMinInstallment()
 
-  const installmentInputValue = +getInstallmentValue();
+  const installmentInputValue = +getInstallmentValue()
 
   state.form.installment =
-    installmentInputValue === "" ||
-    installmentInputValue < state.form.minInstallment
+    installmentInputValue === '' || installmentInputValue < state.form.minInstallment
       ? state.form.minInstallment
-      : installmentInputValue;
+      : installmentInputValue
 
-  const { transaction, installment, minInstallment } = state.form;
+  const { transaction, installment, minInstallment } = state.form
 
-  renderForm(transaction, installment, minInstallment);
-};
+  renderForm(transaction, installment, minInstallment)
+}
 
 // Result controller
 const controlResult = () => {
-  state.result = new Result(state.form.transaction, state.form.installment);
+  state.result = new Result(state.form.transaction, state.form.installment)
 
-  state.result.calculate();
+  state.result.calculate()
   renderResult(
     state.result.transaction,
     state.result.installment,
     state.result.lastInstallment,
     state.result.numberOfInstallments
-  );
-};
+  )
+}
 
 // Event listeners
-DOM.transactionInput.addEventListener("change", () => {
-  controlForm();
-});
+DOM.transactionInput.addEventListener('change', () => {
+  controlForm()
+})
 
-DOM.form.addEventListener("submit", e => {
-  e.preventDefault();
-  controlForm();
-  controlResult();
-});
+DOM.form.addEventListener('submit', e => {
+  e.preventDefault()
+  controlForm()
+  controlResult()
+})
